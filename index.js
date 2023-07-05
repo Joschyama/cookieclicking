aeaea62b617f692e0874361089c176a31528eded
Joschyama <109805410+Joschyama@users.noreply.github.com>
2023-07-05 12:44:02 +0000

    pls

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let score = document.getElementById("score")
-let counter = 0
-function harvest(){
-    counter += 1;
-    score.innerHTML = counter;
-};
+document.addEventListener("DOMContentLoaded", function() {
+    let score = document.getElementById("score");
+    let counter = 0;
+  
+    function harvest() {
+      counter += 1;
+      score.innerHTML = counter;
+    }
+  });
+  
