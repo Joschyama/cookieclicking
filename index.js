0ae023205c0eeaf39a1da4db96ca2e6e3b6e14b6
Joschyama <109805410+Joschyama@users.noreply.github.com>
2023-07-07 07:08:24 +0000

    finally

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
-let score = document.getElementById("score");
+let scores = document.getElementById("score");
 let counter = 0;
   
 function harvest() {
   counter += 1;
   score.innerHTML = counter;
 }
+
+if (counter >= 10);
