f1e6101e1a521cd7ff3be8e2ea7b0959be743aab
Joschyama <109805410+Joschyama@users.noreply.github.com>
2023-07-07 07:20:16 +0000

    first cookie clicking upgrade

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
 let scores = document.getElementById("score");
 let counter = 0;
-  
+let addedvalue = 1;
+
 function harvest() {
-  counter += 1;
+  counter += addedvalue;
   score.innerHTML = counter;
 }
-
-if (counter >= 10);
+function buyupgrade1(){
+  if (counter >= 10) {
+    counter -= 10
+    addedvalue += 1
+    score.innerHTML = counter;
+  }
+};
