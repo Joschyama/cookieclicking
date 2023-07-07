cb62963eb68b27e40a0a70bb4dc0b311b70f276f
Joschyama <109805410+Joschyama@users.noreply.github.com>
2023-07-07 13:43:15 +0000

    added so it shows you if you can buy it or notbut1

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ let scores = document.getElementById("score");
 let counter = 0;
 let addedvalue = 1;
 let autovalue = 0;
-let func = add_autovalue;
-let run = setInterval("func()", 1000);
+let func1 = add_autovalue;
+let run1 = setInterval("func1()", 1000);
+let func2 = enoughcookies;
+let run2 = setInterval("func2()", 10);
+let func3 = notenoughcookies;
+let run3 = setInterval("func3()", 10);
 
+function enoughcookies(){
+  if (counter >= 10) {
+    document.getElementById("butup1").style.backgroundColor = "green";
+  }
+};
+function notenoughcookies(){
+  if (counter < 10) {
+  document.getElementById("butup1").style.backgroundColor = "red";
+  }
+}
 function add_autovalue(){
   counter += autovalue
   score.innerHTML = counter;
